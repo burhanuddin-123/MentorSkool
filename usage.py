4dc03658363535767038b017707b78a64b97e86c
Burhanuddin <burhanuddinnahargarwala@gmail.com>
2021-06-07 18:18:43 +0530

    Package Created and Requirement.txt file added

--- usage.py
+++ usage.py
@@ -1,4 +1,7 @@
-from loader import Loader
+# from loader import loader # Module name loader will be imported from the package loader
+# from loader.loader import Loader   # directly class will be imported
+
+from loader.loader import Loader
 
 l = Loader("C:/Users/Burhanuddin/Study/Slack/Software_Development/products.csv")
 l.load("Github","Product")
