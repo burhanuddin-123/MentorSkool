87f6d2ad51c0e6744627ec8850e0357e2e27c2e6
burhanuddin-123 <58836957+burhanuddin-123@users.noreply.github.com>
2021-07-21 12:47:50 +0530

    Update upload_file.py
    (Only csv file can be uploaded and we can upload file from any directory)-- Done

--- Python_Flask/upload_file.py
+++ Python_Flask/upload_file.py
@@ -2,11 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 from pandas.errors import ParserError
 import csv
-from werkzeug.utils import secure_filename
-import json
-
-# from werkzeug import secure_filename # not available
-# from werkzeug.datastructures import  FileStorage # Explore more about this
+import os
 
 app = Flask(__name__)
 
@@ -17,31 +13,21 @@ def home():
 @app.route('/upload', methods=['GET','POST'])
 def data():
     if request.method == 'POST':
-        file = request.form
-        print(type(file))
-        data = []
-        # with open(f) as file:
-        # csvfile = csv.reader(file)
-        try:
-            csvfile = pd.read_csv(file)
-        except ParserError as err:
-            msg = "File type is Incorrect as appropriate type should be '.csv' or file is corrupted"
-            return render_template('file_upload.html')   #,msg=msg)
-        except ValueError as err:
-            return render_template('file_upload.html')
-        else:
-            for line in csvfile:
-                data.append(line)
+        file = request.files['csv']
+        name = file.filename
+        if name.endswith('.csv'):
+            if not os.path.isdir('static'):
+                os.mkdir('static')
+            filepath = os.path.join('static', file.filename)
+            file.save(filepath)
+            data = []
+            with open(f"static/{name}") as file:
+                csvfile = csv.reader(file)
+                for line in csvfile:
+                    data.append(line)
             return {'data':data}
-        # return render_template('data.html',data=data)
-        # print(type({'data':data})) # TypeError
-        # return render_template('data.html', data=json.dumps(data, indent=2))
-
-# @app.route('/upload', methods=['POST','GET'])
-# def upload():
-#     # if request.method == 'POST':
-#     return "<h1>File Uploaded Successfull !!!</h1>"
-## Here i am facing problem with post method as post doesnot returns string as above
+        else:
+            return render_template('file_upload.html', msg="Not a csv file")  ## mistake as we have to redirect url to home
 
 if __name__ == '__main__':
     app.run(debug=True)
